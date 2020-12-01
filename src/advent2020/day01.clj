57f815315cc0e65b2f5d4f08a87a805f78f4208e
Ken Smith <ken.smith@2scientists.net>
2020-12-01 06:37:01 -0700

    Solution to day01 part2

--- src/advent2020/day01.clj
+++ src/advent2020/day01.clj
@@ -1,5 +1,8 @@
 (ns advent2020.day01
-  (:require [advent2020.lib.utils :as u]))
+  (:require [clojure.math.combinatorics :as combo]
+            [advent2020.lib.utils :as u]))
+
+(def day01-example [1721 979 366 299 675 1456])
 
 (def day01-input
   (map read-string (u/puzzle-input "day01-input.txt")))
@@ -15,9 +18,16 @@
   []
   (apply * (find-pair-that-sums-to-total 2020 day01-input)))
 
-(comment
-  (sort day01-input)
-  (map (partial - 2020) day01-input)
-  (find-pair-that-sums-to-total 2020 day01-input)
-  (day01-part01-soln)
-  (find-triple-that-sums-to-total 2020 day01-input))
+(defn find-triple-that-sums-to-total
+  [total candidates]
+  (let [candidate-set (->> (combo/cartesian-product candidates candidates)
+                           (map (partial apply +))
+                           set)]
+    (->> candidates
+         (map (partial - total))
+         (filter candidate-set)
+         (map (partial - total)))))
+
+(defn day02-part02-soln
+  []
+  (apply * (find-triple-that-sums-to-total 2020 day01-input)))
