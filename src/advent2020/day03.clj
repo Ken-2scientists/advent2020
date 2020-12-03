bfce13c516167ef1788dce1fae98ea306545e3df
Ken Smith <ken.smith@2scientists.net>
2020-12-03 08:37:55 -0700

    Adding tests for day03

--- src/advent2020/day03.clj
+++ src/advent2020/day03.clj
@@ -2,28 +2,14 @@
   (:require [advent2020.lib.ascii :as ascii]
             [advent2020.lib.utils :as u]))
 
-(def day03-sample "..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#")
-
 (def day03-input (u/puzzle-input "day03-input.txt"))
 
-(def forest-mapping
-  {\. :space
-   \# :tree})
-
-(defn forest-base-map
+(defn forest-basemap
   [ascii-lines]
-  (let [height (count ascii-lines)
-        width  (count (first ascii-lines))]
+  (let [forest-mapping {\. :space
+                        \# :tree}
+        height         (count ascii-lines)
+        width          (count (first ascii-lines))]
     {:height height
      :width  width
      :themap (ascii/ascii->map forest-mapping ascii-lines)}))
@@ -48,19 +34,16 @@
 
 (defn day03-part1-soln
   []
-  (let [basemap (forest-base-map day03-input)]
-    (trees-along-slope basemap [3 1])))
+  (let [basemap (forest-basemap day03-input)
+        slope   [3 1]]
+    (trees-along-slope basemap slope)))
 
 (defn trees-along-slopes
   [basemap slopes]
   (map (partial trees-along-slope basemap) slopes))
 
-(defn test1
-  []
-  (let [basemap (forest-base-map (clojure.string/split day03-sample #"\n"))]
-    (trees-along-slopes basemap [[1 1] [3 1] [5 1] [7 1] [1 2]])))
-
 (defn day03-part2-soln
   []
-  (let [basemap (forest-base-map day03-input)]
-    (reduce * (trees-along-slopes basemap [[1 1] [3 1] [5 1] [7 1] [1 2]]))))
+  (let [basemap (forest-basemap day03-input)
+        slopes  [[1 1] [3 1] [5 1] [7 1] [1 2]]]
+    (reduce * (trees-along-slopes basemap slopes))))
