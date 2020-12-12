2042261c1767a9ae237f15620adcd3c57c5554d1
Ken Smith <ken.smith@2scientists.net>
2020-12-12 14:51:38 -0700

    Cleanup of day 10

--- src/advent2020/day10.clj
+++ src/advent2020/day10.clj
@@ -4,70 +4,12 @@
 
 (def day10-input (map read-string (u/puzzle-input "day10-input.txt")))
 
-(def day10-sample
-  (map read-string
-       (str/split
-        "16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4" #"\n")))
-
-(def day10-sample2
-  (map read-string
-       (str/split
-        "28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3" #"\n")))
-
-(sort (conj day10-sample 0))
-(rest (sort (conj day10-sample 0)))
-
 (defn freq-steps
   [jolts]
   (let [l (sort (conj jolts 0))
         fs (frequencies (map - (rest l) l))]
     [(get fs 1)
      (inc (get fs 3))]))
-
-(defn day10-part1-soln
-  []
-  (let [freqs (freq-steps day10-input)]
-    (reduce * freqs)))
 
 (defn combo-mapping
   [run-size]
@@ -86,6 +28,11 @@
          (filter #(> % 1))
          (map combo-mapping)
          (reduce *))))
+
+(defn day10-part1-soln
+  []
+  (let [freqs (freq-steps day10-input)]
+    (reduce * freqs)))
 
 (defn day10-part2-soln
   []
