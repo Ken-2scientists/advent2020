ba9286d2a8b78c0e64e52e1db1100e6c36fe7dfd
Ken Smith <ken.smith@2scientists.net>
2020-12-12 15:19:31 -0700

    Add tests and clean up day 07

--- src/advent2020/day07.clj
+++ src/advent2020/day07.clj
@@ -2,26 +2,6 @@
   (:require [clojure.string :as str]
             [advent2020.lib.utils :as u]))
 
-(def day07-sample (str/split
-                   "light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags." #"\n"))
-
-(def day07-sample2 (str/split
-                    "shiny gold bags contain 2 dark red bags.
-dark red bags contain 2 dark orange bags.
-dark orange bags contain 2 dark yellow bags.
-dark yellow bags contain 2 dark green bags.
-dark green bags contain 2 dark blue bags.
-dark blue bags contain 2 dark violet bags.
-dark violet bags contain no other bags." #"\n"))
-
 (defn keywordize
   [adj color]
   (keyword (str/join "-" [adj color])))
@@ -68,10 +48,14 @@ dark violet bags contain no other bags." #"\n"))
       (* cnt
          (reduce + 1 (map #(satisfy-rule rules (first %) (second %)) needed-bags))))))
 
+(defn count-inner-bags
+  [rules bag]
+  (dec (satisfy-rule (into {} rules) 1 bag)))
+
 (defn day07-part1-soln
   []
   (count (all-outer-bags day07-input :shiny-gold)))
 
 (defn day07-part2-soln
   []
-  (dec (satisfy-rule (into {} day07-input) 1 :shiny-gold)))
+  (count-inner-bags day07-input :shiny-gold))
