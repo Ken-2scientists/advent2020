a5913b960ec6da26c92cbda56cc3ed1c7523e6e6
Ken Smith <ken.smith@2scientists.net>
2020-12-15 04:54:39 -0700

    Day 15 part 1 soln

---
+++ src/advent2020/day15.clj
@@ -0,0 +1,41 @@
+(ns advent2020.day15
+  (:require [clojure.string :as str]
+            [advent2020.lib.utils :as u]))
+
+(def day15-input (map read-string
+                      (-> (u/puzzle-input "day15-input.txt")
+                          first
+                          (str/split #","))))
+
+(defn rules
+  [nums lastnum]
+  (let [positions (nums lastnum)
+        size      (count positions)]
+    (if (= 1 size)
+      0
+      (- (reduce - (subvec positions (- size 2) size))))))
+
+(defn conjv
+  [v x]
+  (if (nil? v)
+    [x]
+    (conj v x)))
+
+(defn memory-seq
+  [starters limit]
+  (let [start-cnt (count starters)]
+    (loop [theseq (vec starters)
+           lastnum (last starters)
+           nums (zipmap starters (mapv vector (range start-cnt)))
+           cnt  start-cnt]
+      (if (<= limit cnt)
+        (take limit theseq)
+        (let [nextnum (rules nums lastnum)]
+          (recur (conj theseq nextnum)
+                 nextnum
+                 (update nums nextnum #(conjv % cnt))
+                 (inc cnt)))))))
+
+(defn day15-part1-soln
+  []
+  (last (memory-seq day15-input 2020)))
