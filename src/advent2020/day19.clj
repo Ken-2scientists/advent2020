c6750f4d973b0df79621db518d5de9c885a4b3d6
Ken Smith <ken.smith@2scientists.net>
2020-12-19 09:52:50 -0700

    Day 19 part 2 soln

--- src/advent2020/day19.clj
+++ src/advent2020/day19.clj
@@ -22,59 +22,58 @@
     {:rules (into {} (map parse-rule (str/split rules #"\n")))
      :messages (str/split messages #"\n")}))
 
-(def day19-sample
-  (parse
-   "0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: \"a\"
-5: \"b\"
-
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb"))
-
 (def day19-input (parse (str/join "\n" (u/puzzle-input "day19-input.txt"))))
-((:rules day19-sample) 0)
+
+(defn handle-special-eleven
+  "I'm not proud of this, but it works"
+  [[left right]]
+  (str "("
+       left right
+       "|"
+       left left right right
+       "|"
+       left left left right right right
+       "|"
+       left left left left right right right right
+       "|"
+       left left left left left right right right right right
+       ")"))
 
 (defn resolve-rule
-  [rules rule]
-  (let [r (rules rule)]
-    (if (string? r)
-      r
-      (if (coll? (first r))
-        (str "("
-             (str/join (map (partial resolve-rule rules) (first r)))
-             "|"
-             (str/join (map (partial resolve-rule rules) (second r)))
-             ")")
-        (str/join (map (partial resolve-rule rules) r))))))
+  ([rules rule]
+   (resolve-rule false rules rule))
+  ([special? rules rule]
+   (let [r (rules rule)]
+     (if (string? r)
+       r
+       (if (coll? (first r))
+         (str "("
+              (str/join (map (partial resolve-rule special? rules) (first r)))
+              "|"
+              (str/join (map (partial resolve-rule special? rules) (second r)))
+              ")")
+         (let [pieces (map (partial resolve-rule special? rules) r)]
+           (if (not special?)
+             (str/join pieces)
+             (case rule
+               8 (str (str/join pieces) "+")
+               11 (handle-special-eleven pieces)
+               (str/join pieces)))))))))
 
 (defn count-matches
-  [{:keys [rules messages]}]
-  (let [pattern (re-pattern (str "^" (resolve-rule rules 0) "$"))]
-    (->> (map (partial re-find pattern) messages)
-         (map first)
-         (filter some?)
-         count)))
+  ([input]
+   (count-matches input false))
+  ([{:keys [rules messages]} special?]
+   (let [pattern (re-pattern (str "^" (resolve-rule special? rules 0) "$"))]
+     (->> (map (partial re-find pattern) messages)
+          (map first)
+          (filter some?)
+          count))))
 
 (defn day19-part1-soln
   []
   (count-matches day19-input))
 
-
-(defn dependencies
-  [rules]
-  (->> (mapcat
-        (fn [[k v]]
-          (when (coll? v)
-            (println v)
-            (map
-             (fn [i]
-               [i k]) (set (flatten v)))))
-        rules)
-       (group-by first)
-       (u/fmap (partial map second))))
+(defn day19-part2-soln
+  []
+  (count-matches day19-input true))
