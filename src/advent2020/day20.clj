19e0a3140e3a6b1ebd7bb5246203f33f55793e0a
Ken Smith <ken.smith@2scientists.net>
2020-12-28 11:27:06 -0700

    WIP on answering part 2

--- src/advent2020/day20.clj
+++ src/advent2020/day20.clj
@@ -132,9 +132,12 @@ Tile 3079:
        (str/join "\n")
        parse))
 
-(defn edge-val
+(defn edge-hash
   [grid edge-indices]
-  (str/join (map grid edge-indices)))
+  (let [hash (str/join (map grid edge-indices))]
+    (if (>= (compare hash (str/reverse hash)) 0)
+      hash
+      (str/reverse hash))))
 
 (defn tile-edges
   [[tile-id {:keys [width height grid]}]]
@@ -144,33 +147,93 @@ Tile 3079:
                      (map vector (repeat (dec width)) (range height))]]
     (map vector
          [[tile-id :t] [tile-id :b] [tile-id :l] [tile-id :r]]
-         (map (partial edge-val grid) edge-coords))))
+         (map (partial edge-hash grid) edge-coords))))
 
-(defn edge-compare
-  [[id1 edge1] [id2 edge2]]
-  (cond (= edge1 edge2) [[id1 id2] :match]
-        (= edge1 (str/reverse edge2)) [[id1 id2] :rev-match]))
+(defn tile-edge-map
+  [tiles]
+  (into {} (mapcat tile-edges tiles)))
 
-(defn edge-matches
-  [all-edges [[tile-id _] :as edge]]
-  (let [other-edges (filter #(not= tile-id (first (key %))) all-edges)]
-    (map (partial edge-compare edge) other-edges)))
+(defn edge-pairs
+  [tile-edge-map]
+  (let [half-matches (->> tile-edge-map
+                          (group-by second)
+                          (filter #(= 2 (count (val %))))
+                          vals
+                          (mapv (partial mapv first)))
+        all-matches (concat half-matches (mapv (comp vec reverse) half-matches))]
+    (into {} all-matches)))
 
-(defn all-edge-matches
-  [input]
-  (let [edges (into {} (mapcat tile-edges input))]
-    (into {} (filter some? (mapcat (partial edge-matches edges) edges)))))
-
-(defn match-counts-per-tile
-  [matches]
-  (u/fmap count (group-by (comp first ffirst) matches)))
+(defn matching-edge-count
+  [tiles]
+  (->> tiles
+       tile-edge-map
+       edge-pairs
+       (group-by ffirst)
+       (u/fmap count)))
 
 (defn corners
-  [input]
-  (let [match-counts (match-counts-per-tile (all-edge-matches input))]
+  [tiles]
+  (let [match-counts (matching-edge-count tiles)]
     (->> (filter #(= 2 (second %)) match-counts)
          (map first))))
 
 (defn day20-part1-soln
   []
   (reduce * (corners day20-input)))
+
+(defn fliph
+  [{:keys [width grid] :as tile}]
+  (assoc tile :grid (u/kmap (fn [[x y]] [(- (dec width) x) y]) grid)))
+
+(defn flipv
+  [{:keys [height grid] :as tile}]
+  (assoc tile :grid (u/kmap (fn [[x y]] [x (- (dec height) y)]) grid)))
+
+(defn rotate
+  [{:keys [width height grid]}]
+  (let [mapping (fn [[x y]] [(- (dec height) y) x])]
+    {:width height
+     :height width
+     :grid (u/kmap mapping grid)}))
+
+;; (filter #(= 1951 ((comp first ffirst) %)) (all-edge-matches day20-sample))
+;; 
+(def grid-sample
+  {:width 2
+   :height 4
+   :grid {[0 0] :a [1 0] :b
+          [0 1] :c [1 1] :d
+          [0 2] :e [1 2] :f
+          [0 3] :g [1 3] :h}})
+
+;; (defn tile-positions-and-orientations
+;;   [input]
+;;   (let [corners (corners input)
+;;         upper-left (first corners)]))
+
+(defn is-edge?
+  [width height [x y]]
+  (or (= 0 x)
+      (= 0 y)
+      (= (dec width) x)
+      (= (dec height) y)))
+
+(defn trim-edge
+  [{:keys [width height grid]}]
+  (let [is-not-edge? (complement (partial is-edge? width height))]
+    {:width (- width 2)
+     :height (- height 2)
+     :grid (into {} (filter (comp is-not-edge? key) grid))}))
+
+(defn combine
+  [])
+
+;; (defn final-image
+;;   [tiles]
+;;   (let [[orientations offsets] (tile-positions-and-orientations tiles)
+;;         corrected-tiles (->> tiles
+;;                              (u/fmap trim-edge)
+;;                              (map orientations)
+;;                              (map offsets)
+;;                              combine)]))
+
--- test/advent2020/day20_test.clj
+++ test/advent2020/day20_test.clj
@@ -117,6 +117,66 @@ Tile 3079:
     (is (= '(1171 2971 3079 1951)
            (t/corners day20-sample)))))
 
+(def grid-sample1
+  {:width 2
+   :height 4
+   :grid {[0 0] :a [1 0] :b
+          [0 1] :c [1 1] :d
+          [0 2] :e [1 2] :f
+          [0 3] :g [1 3] :h}})
+
+(def grid-sample2
+  {:width 3
+   :height 3
+   :grid {[0 0] :a [1 0] :b [2 0] :c
+          [0 1] :d [1 1] :e [2 1] :f
+          [0 2] :e [1 2] :f [2 2] :g}})
+
+(deftest flips-and-rotations
+  (testing "Horizontal Flips"
+    (is (= {:width 2
+            :height 4
+            :grid {[0 0] :b [1 0] :a
+                   [0 1] :d [1 1] :c
+                   [0 2] :f [1 2] :e
+                   [0 3] :h [1 3] :g}}
+           (t/fliph grid-sample1)))
+    (is (= grid-sample1 (t/fliph (t/fliph grid-sample1)))))
+  (testing "Vertical Flips"
+    (is (= {:width 2
+            :height 4
+            :grid {[0 0] :g [1 0] :h
+                   [0 1] :e [1 1] :f
+                   [0 2] :c [1 2] :d
+                   [0 3] :a [1 3] :b}}
+           (t/flipv grid-sample1)))
+    (is (= grid-sample1 (t/flipv (t/flipv grid-sample1)))))
+  (testing "Rotations"
+    (is (= {:width 4
+            :height 2
+            :grid {[0 0] :g [1 0] :e [2 0] :c [3 0] :a
+                   [0 1] :h [1 1] :f [2 1] :d [3 1] :b}}
+           (t/rotate grid-sample1)))
+    (is (= {:width 2
+            :height 4
+            :grid {[0 0] :h [1 0] :g
+                   [0 1] :f [1 1] :e
+                   [0 2] :d [1 2] :c
+                   [0 3] :b [1 3] :a}}
+           (t/rotate (t/rotate grid-sample1))))
+    (is (= {:width 4
+            :height 2
+            :grid {[0 0] :b [1 0] :d [2 0] :f [3 0] :h
+                   [0 1] :a [1 1] :c [2 1] :e [3 1] :g}}
+           (t/rotate (t/rotate (t/rotate grid-sample1)))))))
+
+(deftest tile-assembly
+  (testing "Trim edges"
+    (is (= {:width 1
+            :height 1
+            :grid {[1 1] :e}}
+           (t/trim-edge grid-sample2)))))
+
 (deftest day20-part1-soln
   (testing "Reproduces the answer for day20, part1"
     (is (= 79412832860579 (t/day20-part1-soln)))))
